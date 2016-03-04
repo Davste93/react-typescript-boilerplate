a2d41f8a0f7050e40456d88e01db20e692c6fb3f
David <dstellini@nxpay.eu>
2016-03-04 16:18:24 +0100

    Updated with typings, simplified

--- app/index.tsx
+++ app/index.tsx
@@ -1,42 +1,18 @@
-/// <reference path="../typings/tsd.d.ts" />
+import {Model, indexKey} from 'tsmvc';
 
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Store, createStore } from 'redux';
-import { Provider } from 'react-redux';
+export class Zobbi extends Model  {
+  @indexKey
+  public id: string;
 
-import { App } from './components/app';
-import { counterApp } from './reducers';
-
-declare const require: (name: String) => any;
-
-interface IHotModule {
-  hot?: { accept: (path: string, callback: () => void) => void };
-};
-
-declare const module: IHotModule;
-
-function configureStore(): Store {
-  const store: Store = createStore(counterApp);
-
-  if (module.hot) {
-    module.hot.accept('./reducers', () => {
-      const nextRootReducer: any = require('./reducers').counterApp;
-      store.replaceReducer(nextRootReducer);
-    });
+  public get size(): number
+  {
+    throw new Error('Stack overflow exception');
   }
 
-  return store;
-}
-
-const store: Store = configureStore();
-
-class Main extends React.Component<{}, {}> {
-  public render(): React.ReactElement<Provider> {
-    return (<Provider store={store}>
-      <App />
-    </Provider>);
+  public create(): Model {
+    return new Zobbi();
   }
+
 }
 
-ReactDOM.render(<Main />, document.getElementById('app'));
+console.log(new Zobbi().size);
