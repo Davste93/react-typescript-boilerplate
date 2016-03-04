5218ed1c6200b2e343ab5b10800134830432bb40
David <dstellini@nxpay.eu>
2016-03-04 17:40:11 +0100

    Updated typings.json

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,18 @@
 import {Model, indexKey} from 'tsmvc';
 
-export class Zobbi extends Model  {
+export class Building extends Model  {
   @indexKey
   public id: string;
 
   public get size(): number
   {
-    throw new Error('Stack overflow exception');
+    return 20;
   }
 
   public create(): Model {
-    return new Zobbi();
+    return new Building();
   }
 
 }
 
-console.log(new Zobbi().size);
+console.log(new Building().size);
