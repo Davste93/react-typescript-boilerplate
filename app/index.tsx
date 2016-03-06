6fb2738552e0c32c60b4370b1a766456237efe0b
dstellini <davste93@gmail.com>
2016-03-06 02:02:50 +0100

    experimenting with jsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,19 @@
 import {Config, Services} from "./tsmvc-consumer/implementation/config/config";
-import * as n from "angular2/core";
+//import * as n from "angular2/core";
+//import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {Table} from "./components/table";
+
+//Create table component
+<div id="app"></div>
+
 var user = Services.userService.getCompleteUser('1').then((user) => {
+  ReactDOM.render(
+    <Table data={user.addresses}/>,
+    document.getElementById('content')
+  );
+  React.createElement(Table({data: user.addresses}), null)
+  ReactDOM.render(, document);
   console.log("got your addresses:" + user.addresses);
 });
